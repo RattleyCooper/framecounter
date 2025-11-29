98b1fdaaf4aca1e1beebae3dc75814ef79499740
py-am-i <6770988+RattleyCooper@users.noreply.github.com>
2025-11-28 23:41:30 -0800

    Add support for callback cancellation and condition-based callback scheduling.

--- src/framecounter.nim
+++ src/framecounter.nim
@@ -3,43 +3,60 @@ export times
 
 type
   OneShot* = ref object
+    id*: int
     body*: proc() {.closure.}
     frame*: uint
     target*: uint
 
   MultiShot* = ref object
+    id*: int
     body*: proc() {.closure.}
     target*: uint
 
   FrameCounter* = ref object
+    nextId*: int
     frame*: uint
     frameProcs*: seq[MultiShot]
     oneShots*: seq[OneShot]
     last*: MonoTime
     fps*: int
 
+proc clear*(framecounter: FrameCounter) =
+  # Clear the closures from the framecounter.
+  framecounter.frameProcs.setLen(0)
+  framecounter.oneShots.setLen(0)
+
+proc genId*(framecounter: FrameCounter): int =
+  # Create an id for the next registered closure.
+  result = framecounter.nextId
+  inc framecounter.nextId
+
 proc frameTime*(frames: int): int =
   # Calculate frames per second.
   ((1 / frames) * 1000).int
 
-template ControlFlow*(f: var FrameCounter) =
+template ControlFlow*(f: FrameCounter) =
   if (getMonoTime() - f.last).inMilliseconds < frameTime(f.fps):
     return
 
-proc tick*(f: var FrameCounter, controlFlow: bool = true) =
+proc tick*(f: FrameCounter, controlFlow: bool = true) =
   if controlFlow:
     f.ControlFlow()
 
   # MultiShots - every
-  for ms in f.frameProcs:
-    if f.frame mod ms.target == 0:
-      ms.body()
+  for i in 0..f.frameProcs.high: # maintain execution order
+    if i > f.frameProcs.high:
+      break
+    if f.frame mod f.frameProcs[i].target == 0 and f.frame != 0:
+      f.frameProcs[i].body()
 
   # OneShots - after
   var c = 0
-  for i in 0..f.oneShots.high:
+  for i in 0..f.oneShots.high: # maintain execution order
+    if c > f.oneShots.high:
+      break
     var osh = f.oneShots[c]
-    if osh.frame >= osh.target:
+    if osh.frame mod osh.target == 0 and osh.frame != 0:
       f.oneShots[c].body()
       f.oneShots.del(c)
     else:
@@ -55,7 +72,7 @@ proc tick*(f: var FrameCounter, controlFlow: bool = true) =
 proc after*(frames: int, body: proc() {.closure.}): OneShot =
   OneShot(
     target: frames.uint,
-    frame: 1,
+    frame: 0,
     body: body
   )
 
@@ -65,11 +82,113 @@ proc every*(frames: int, body: proc() {.closure.}): MultiShot =
     body: body
   )
 
-proc run*(f: var FrameCounter, a: OneShot) =
+proc run*(f: FrameCounter, a: OneShot) =
+  a.id = f.genId()
   f.oneShots.add a
 
-proc run*(f: var FrameCounter, e: MultiShot) =
+proc run*(f: FrameCounter, e: MultiShot) =
+  e.id = f.genId()
   f.frameProcs.add e
+
+proc schedule*(f: FrameCounter, a: OneShot): int =
+  # Same as run, but returns the id you can use to cancel the closure.
+  a.id = f.genId()
+  f.oneShots.add a
+  a.id
+
+proc schedule*(f: FrameCounter, e: MultiShot): int =
+  # Same as run, but returns the id you can use to cancel the closure.
+  e.id = f.genId()
+  f.frameProcs.add e
+  e.id
+
+proc cancel*(f: FrameCounter, id: int) =
+  # Removes closures from the framecounter.
+  # Remove from OneShots
+  for i in countdown(f.oneShots.high, 0):
+    if f.oneShots[i].id == id:
+      f.oneShots.del(i)
+      return
+  # Remove from MultiShots
+  for i in countdown(f.frameProcs.high, 0):
+    if f.frameProcs[i].id == id:
+      f.frameProcs.del(i)
+      return
+
+proc cancel*(f: FrameCounter, ids: var seq[int]) =
+  ## Batch cancellation. Removes all tasks in the list and clears the list.
+  ## Usage: clock.cancel(player.tasks)
+  for i in countdown(ids.high, 0):
+    f.cancel(ids[i])
+  ids.setLen(0)
+
+proc watch*(f: FrameCounter, cond: proc(): bool {.closure.}, m: MultiShot): int =
+  let id = f.nextId
+  f.run every(1) do():
+    if cond():
+      m.id = f.genId()
+      f.frameProcs.add m
+      f.cancel(id)
+  id
+
+proc watch*(f: FrameCounter, cond: proc(): bool {.closure.}, o: OneShot): int =
+  let id = f.nextId
+  f.run every(1) do():
+    if cond():
+      o.id = f.genId()
+      f.oneShots.add o
+      f.cancel(id)
+  id
+
+template watch*(f: FrameCounter, cond: untyped, m: MultiShot): untyped =
+  let id = f.nextId
+  f.run every(1) do():
+    if (`cond`):
+      m.id = f.genId()
+      f.frameProcs.add m
+      f.cancel(id)
+  id
+
+template watch*(f: FrameCounter, cond: untyped, o: OneShot): untyped =
+  let id = f.nextId
+  f.run every(1) do():
+    if (`cond`):
+      o.id = f.genId()
+      f.oneShots.add o
+      f.cancel(id)
+  id
+
+proc `when`*(f: FrameCounter, cond: proc(): bool {.closure.}, m: MultiShot) =
+  let id = f.nextId
+  f.run every(1) do():
+    if cond():
+      m.id = f.genId()
+      f.frameProcs.add m
+      f.cancel(id)
+
+proc `when`*(f: FrameCounter, cond: proc(): bool {.closure.}, o: OneShot) =
+  let id = f.nextId
+  f.run every(1) do():
+    if cond():
+      o.id = f.genId()
+      f.oneShots.add o
+      f.cancel(id)
+
+template `when`*(f: FrameCounter, cond: untyped, m: MultiShot): untyped =
+  let id = f.nextId
+  f.run every(1) do():
+    if (`cond`):
+      m.id = f.genId()
+      f.frameProcs.add m
+      f.cancel(id)
+
+template `when`*(f: FrameCounter, cond: untyped, o: OneShot): untyped =
+  let id = f.nextId
+  f.run every(1) do():
+    if (`cond`):
+      o.id = f.genId()
+      f.oneShots.add o
+      f.cancel(id)
 
 proc newFrameCounter*(fps: int): FrameCounter =
   var f: FrameCounter
@@ -82,10 +201,13 @@ proc newFrameCounter*(fps: int): FrameCounter =
   return f
 
 if isMainModule:
+  var clock = FrameCounter(fps:60)
+
   type 
     Cat = ref object
       name: string
       age: int
+      tasks: seq[int] # Our bag of tasks
   
   proc newCat(name: string): Cat =
     # Create a new cat.
@@ -93,38 +215,54 @@ if isMainModule:
     result.name = name
     result.age = 1
 
-  var clock = FrameCounter(fps: 60)
+  # var clock = FrameCounter(fps: 60)
   var scrubs = newCat("Scrubs")
-  var frank = newCat("Frank")
+  var shadow = newCat("Shadow")
 
-  # Closure will capture `c`, `scrubs`, and `frank`, 
-  # for use in the closure.
+  # Closure will capture `c`, `scrubs`, and `shadow`, for use in the closure.
   # At 60fps, every(60) means this runs once per second.
   var c = 0
   clock.run every(60) do():
+    if c == 5:
+      # Cancel scrubs' rapid aging
+      clock.cancel(scrubs.tasks)
     if c == 10:
       quit(QuitSuccess)
     c += 1
     echo "C: ", c
     echo scrubs.age
     echo scrubs.name
-    echo frank.age
-    echo frank.name
+    echo shadow.age
+    echo shadow.name
     echo ""
 
+  proc ageInc(cat: Cat) =
+    # Testable without framecounter
+    cat.age += 1
 
-  proc doStuff(cat: Cat) =
+  proc nameChange(cat: Cat, name: string) =
+    # Testable without framecounter
+    cat.name = name
+    echo cat.name, " got a new name!"
+
+  proc setupCat(cat: Cat) =
     # Create a closure inside a proc for scheduling code
     # on multiple objects.
-    clock.run every(60) do():
-      cat.age += 1
+    clock.run every(60) do(): 
+      cat.ageInc()
     # After 3 seconds (180 frames at 60fps), rename the cat
     clock.run after(180) do():
-      cat.name = "Mr. " & cat.name
-      echo cat.name, " got a new name!"
+      cat.nameChange("Mr. " & cat.name)
 
-  scrubs.doStuff()
-  frank.doStuff()
+  scrubs.setupCat()
+  shadow.setupCat()
+
+  # Scrubs will age rapidly until task is canceled
+  scrubs.tasks.add clock.schedule every(60) do():
+    scrubs.age += 1
+
+  clock.when scrubs.age > 5, after(60) do():
+    echo "Scrubs is old!"
 
   while true:
     clock.tick()
